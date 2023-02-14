d1c08b09867f479ec59d4bfe4efa21c29d2a7315
Aida <edigeeva.aika@mail.ru>
2023-02-14 15:47:37 +0600

    changed object data

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 let exasmple1 = {};
-let joldosh  = {name : "Joldosh",age: 22, isEmployed: false};
+let joldosh  = {name : "Joldosh",age: 22, isEmployed: false,sayHello:function(){
+  console.log("Hello, I'm " + this.name);
+ },};
+
+ joldosh.age = 23;
+ console.log (joldosh.age);
+ joldosh.favouriteBooks =["Harry Potor","Harry Potor-2"];
+ aboutmyself.sayHello();
+ joldosh.name = "Jol";
+ joldosh.sayHello();
+ 
+
 let aboutmyself = {   
   name : "Aida",
   age: 36,
   lastname: "Edigeeva",
   status: "merried",
   children: 3,
-  sayHello:function(){},
-  console.log("Hello");
+  sayHello:function(){
+  console.log("Hello, I'm " + this.name);
+ },
+ acodeword:function(){
+  console.log('cup-cup');
+ },
 };
+console.log(aboutmyself.acodeword);
 console.log(aboutmyself.age);
 
 //view ,edit,add
 console.log(joldosh.name);
 
 
-joldosh.age = 23;
-console.log (joldosh.age);
-joldosh.favouriteBooks =["Harry Potor","Harry Potor-2"];
 
 //delete
-delete joldosh.favouriteBooks;
-console.log(joldosh.favouriteBooks);
-
-
+// delete joldosh.favouriteBooks;
+// console.log(joldosh.favouriteBooks);
 
 // aboutmyself.children = 5;
 // console.log (aboutmyself.children);
