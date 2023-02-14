d66bca0d018bb0b95d1b70e02d67a8d08e593b62
Aida <edigeeva.aika@mail.ru>
2023-02-14 15:11:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let exasmple1 = {};
+let joldosh  = {name : "Joldosh",age: 22, isEmployed: false};
+let aboutmyself = {   
+  name : "Aida",
+  age: 36,
+  lastname: "Edigeeva",
+  status: "merried",
+  children: 3,
+};
+console.log(aboutmyself.age);
+
+//view ,edit,add
+console.log(joldosh.name);
+joldosh.age = 23;
+console.log (joldosh.age);
+joldosh.favouriteBooks =["Harry Potor","Harry Potor-2"];
+
+//delete
+delete joldosh.favouriteBooks;
+console.log(joldosh.favouriteBooks);
+
+
+// aboutmyself.children = 5;
+// console.log (aboutmyself.children);
